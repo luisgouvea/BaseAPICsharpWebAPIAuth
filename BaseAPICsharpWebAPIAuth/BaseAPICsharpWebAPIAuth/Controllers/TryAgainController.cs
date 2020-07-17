c38247c66f168dcf1dadd99ac416467582e0afac
Luis Eduardo <924365@lojasrenner.com.br>
2020-07-17 14:34:20 -0300

    initial commit

---
+++ BaseAPICsharpWebAPIAuth/BaseAPICsharpWebAPIAuth/Controllers/TryAgainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BaseAPICsharpWebAPIAuth.Controllers
+{
+    public class TryAgainController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage seila()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, true);
+        }
+    }
+}
